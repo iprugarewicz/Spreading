900f8c1cecc9556c6c20d22b4119c40ac3a9326d
Ignacy Prugarewicz <ignacypr@gmail.com>
2020-04-07 22:58:52 +0200

    auto spreading circle easier velocity vector control

--- spreading.py
+++ spreading.py
@@ -9,15 +9,14 @@ pygame.init()
 angle = 0
 elements = 100
 els = []
-v = 0.5
+v = 0.1
 sizex = 500
 sizey = 500
 screen = pygame.display.set_mode((sizex, sizey))
 
 for i in range(elements):
     els.append(
-        [sizex / 2, sizey / 2, v * math.cos(i * 2 * math.pi / elements), v * math.sin(i * 2 * math.pi / elements),
-         False])
+        [sizex / 2, sizey / 2, math.pi * 2 * i / elements, False])
 
 while True:
     for event in pygame.event.get():
@@ -28,8 +27,8 @@ while True:
     screen.fill((0, 0, 0))
     for i, j in zip(els, range(len(els))):
         pygame.draw.circle(screen, (255, 255, 0), (int(i[0]), int(i[1])), 2)
-        els[j][0] += i[2]
-        els[j][1] += i[3]
+        els[j][0] += v * math.cos(i[2])
+        els[j][1] += v * math.sin(i[2])
 
     pygame.display.flip()
     time.sleep(0.011)
