a759779bcda7de6f7626e26e976c0ee911f1fe3e
Ignacy Prugarewicz <ignacypr@gmail.com>
2020-04-08 11:22:20 +0200

    border bounce works

--- spreading.py
+++ spreading.py
@@ -6,8 +6,7 @@ import time
 
 pygame.init()
 
-angle = 0
-elements = 100
+elements = 10000
 els = []
 v = 1
 sizex = 500
@@ -19,18 +18,19 @@ def curve(alpha):
     return alpha + ((math.pi / 4) * ((random.randrange(101) + random.randrange(101)) / 201)) - (math.pi / 8)
 
 
-def border(alpha):
-    if (math.pi / 4) > alpha >= 0 or (2 * math.pi) > alpha >= (7 * math.pi / 8):
-        return math.pi - alpha
+def border(elem):
+    if elem[0] >= sizex - 1:
+        return (math.pi - elem[2])
+    elif elem[0] <= 1:
+        return (math.pi - elem[2])
+    elif elem[1] >= sizey - 1:
+        return (2 * math.pi - elem[2])
+    elif elem[1] <= 1:
+        return (2 * math.pi - elem[2])
+    else:
+        return elem[2]
 
-    elif (3 * math.pi / 4) > alpha >= (math.pi / 4):
-        return math.pi + (math.pi - alpha)
 
-    elif (5 * math.pi / 4) > alpha >= (3 * math.pi / 4):
-        return (3 * math.pi) - alpha
-
-    elif (7 * math.pi / 8) > alpha >= (5 * math.pi / 4):
-        return math.pi - alpha
 
 for i in range(elements):
     els.append(
@@ -47,11 +47,9 @@ while True:
         pygame.draw.circle(screen, (255, 255, 0), (int(i[0]), int(i[1])), 2)
         els[j][0] += v * math.cos(i[2])
         els[j][1] += v * math.sin(i[2])
-        if els[j][0] <= 1 or els[j][0] >= sizex - 1 or els[j][1] <= 1 or els[j][1] >= sizey - 1:
-            els[j][2] = border(els[j][2]) % (math.pi * 2)
-            els[j][0] = els[j][0] % sizex
-            els[j][1] = els[j][1] % sizey
+        els[j][2] = border(els[j])
+
         # els[j][2] = curve(els[j][2])%(math.pi*2)
 
     pygame.display.flip()
-    time.sleep(0.011)
+    time.sleep(0.001)
