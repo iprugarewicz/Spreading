abb716d1c106256920aeeccecff8c2043fc7a442
Ignacy Prugarewicz <ignacypr@gmail.com>
2020-04-19 22:59:16 +0200

    spreading works, but not very effective Too much calculations

--- spreading.py
+++ spreading.py
@@ -6,11 +6,11 @@ import time
 
 pygame.init()
 
-elements = 1000
+elements = 100
 els = []
 v = 1
-sizex = 500
-sizey = 700
+sizex = 300
+sizey = 300
 screen = pygame.display.set_mode((sizex, sizey))
 pygame.display.set_caption('Virus spreading simulation')
 
@@ -31,11 +31,10 @@ def border(elem):
         return elem[2]
 
 
-
 for i in range(elements):
     els.append(
         [sizex / 2, sizey / 2, math.pi * 2 * i / elements, False, (255, 255, 0)])
-
+els[0][3] = True
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,13 +43,27 @@ while True:
             sys.exit(0)
     screen.fill((0, 0, 0))
     for i, j in zip(els, range(len(els))):
-        pygame.draw.circle(screen, i[4], (int(i[0]), int(i[1])), 2)
+
+        # position
         els[j][0] += v * math.cos(i[2])
         els[j][1] += v * math.sin(i[2])
 
+        # angle
         els[j][2] = curve(els[j][2]) % (math.pi * 2)
 
+        # border bounce
         els[j][2] = border(els[j])
+
+        # spreading
+        if time.clock() >= 5.0:
+            for elem in els:
+                if i[3] == True and ((elem[0] - i[0]) ** 2) + ((elem[1] - i[1]) ** 2) <= 5:
+                    els[els.index(elem)][3] = True
+        # coloring
+        if i[3] == True:
+            els[j][4] = (255, 0, 0)
+
+        pygame.draw.circle(screen, i[4], (int(i[0]), int(i[1])), 2)
 
     pygame.display.flip()
     time.sleep(0.01)
