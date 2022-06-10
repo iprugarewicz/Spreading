cd87c0f74342a2be2e745972e87e1e72a53184a1
ignacy <ignacypr@gmail.com>
2022-06-10 02:10:49 +0200

    improvements

--- spreading.py
+++ spreading.py
@@ -4,7 +4,6 @@ import random
 import math
 import time
 
-
 pygame.init()
 
 sim_time = 0
@@ -16,22 +15,23 @@ v = 1
 sizeX = 400
 sizeY = 400
 
-screen = pygame.display.set_mode((2*sizeX, sizeY))
+screen = pygame.display.set_mode((2 * sizeX, sizeY))
 pygame.display.set_caption('Virus spreading simulation')
+
 
 def curve(alpha):
     return alpha + ((math.pi / 4) * ((random.randrange(101) + random.randrange(101)) / 201)) - (math.pi / 8)
 
 
-def border(elem):
-    if elem[0] >= sizeX - 1 or elem[0] <= 1:
-        return math.pi - elem[2]
-    elif elem[1] >= sizeY - 1:
-        return 2 * math.pi - elem[2]
-    elif elem[1] <= 1:
-        return 2 * math.pi - elem[2]
+def border(element):
+    if element[0] >= sizeX - 1 or element[0] <= 1:
+        return math.pi - element[2]
+    elif element[1] >= sizeY - 1:
+        return 2 * math.pi - element[2]
+    elif element[1] <= 1:
+        return 2 * math.pi - element[2]
     else:
-        return elem[2]
+        return element[2]
 
 
 for i in range(element_count):
@@ -44,7 +44,7 @@ while True:
             sys.exit(0)
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
-    pygame.draw.rect(screen,(0,0,0),pygame.Rect(0,0,sizeX+3,sizeY))
+    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 0, sizeX + 3, sizeY))
 
     for i, j in zip(elements, range(len(elements))):
 
@@ -61,9 +61,9 @@ while True:
         # spreading
         if sim_time >= 100:
             for elem in elements:
-                if i[3] == True and ((elem[0] - i[0]) ** 2) + ((elem[1] - i[1]) ** 2) <= 5:
-                    if elements[elements.index(elem)][3]==False:
-                        infected+=1
+                if i[3] and ((elem[0] - i[0]) ** 2) + ((elem[1] - i[1]) ** 2) <= 5:
+                    if not elements[elements.index(elem)][3]:
+                        infected += 1
                     elements[elements.index(elem)][3] = True
 
         # coloring
@@ -72,8 +72,8 @@ while True:
 
         pygame.draw.circle(screen, i[4], (int(i[0]), int(i[1])), 2)
 
-    pygame.draw.circle(screen, (255,255,255), ((sizeX+(sim_time/5)%sizeX),sizeY- sizeY*(infected/element_count)),1)
+    pygame.draw.circle(screen, (255, 255, 255),
+                       ((sizeX + (sim_time / 5) % sizeX), sizeY - sizeY * (infected / element_count)), 1)
 
     pygame.display.flip()
-    time.sleep(0.01)
-    sim_time+=1
+    sim_time += 1
